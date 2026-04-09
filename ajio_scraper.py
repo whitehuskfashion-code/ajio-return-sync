90f646bf76dec1bf0d61e8645b03af513ac40be3
whitehuskfashion-code <whitehuskfashion@gmail.com>
2026-04-09 20:25:16 +0530

    Update ajio_scraper.py

--- ajio_scraper.py
+++ ajio_scraper.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 
 logger = logging.getLogger(__name__)
 LOGIN_URL   = "https://seller.ajio.com/ajiocommerce/"
